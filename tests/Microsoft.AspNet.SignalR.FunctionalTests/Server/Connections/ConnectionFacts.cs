5d06b2838ec1ff9fe629c3332aba8d5c4d1ced08
NTaylorMullen <NTaylorMullen@outlook.com>
2013-05-28 12:43:41 -0700

    Added the ability for client's to override the servers transportConnectTimeout value.
    - Also added tests to verify the new functionality.
    
    #1889

--- tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/ConnectionFacts.cs
+++ tests/Microsoft.AspNet.SignalR.FunctionalTests/Server/Connections/ConnectionFacts.cs
@@ -105,7 +105,36 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                         Assert.NotNull(ser.Exception);
                     }
                 }
-            }            
+            }
+
+            [Fact]
+            public void ConnectionUsesClientSetTransportConnectTimeout()
+            {
+                using (var host = new MemoryHost())
+                {
+                    host.Configure(app =>
+                    {
+                        var config = new ConnectionConfiguration
+                        {
+                            Resolver = new DefaultDependencyResolver()
+                        };
+
+                        app.MapConnection<MyConnection>("echo", config);
+                    });
+
+                    var tcs = new TaskCompletionSource<string>();
+                    var connection = new Connection("http://foo/echo");
+                    var newTimeout = TimeSpan.FromSeconds(4);
+
+                    using (connection)
+                    {
+                        Assert.False(connection.TransportConnectTimeout.HasValue);
+                        connection.TransportConnectTimeout = newTimeout;
+                        connection.Start(host).Wait();
+                        Assert.Equal(connection.TransportConnectTimeout.Value, newTimeout);
+                    }
+                }
+            }
 
             [Fact]
             public void FallbackToLongPollingIIS()
@@ -113,7 +142,7 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
                 using (ITestHost host = CreateHost(HostType.IISExpress))
                 {
                     // Reduce transportConnectionTimeout to 3 seconds
-                    host.Initialize(-1,110,30,3);
+                    host.Initialize(transportConnectTimeout: 3);
 
                     var connection = CreateConnection(host, "/fall-back");
 
@@ -131,9 +160,9 @@ namespace Microsoft.AspNet.SignalR.Client.Tests
 
                         var client = new DefaultHttpClient();
                         var transports = new IClientTransport[]  {
-                        new ServerSentEventsTransport(client),
-                        new LongPollingTransport(client)
-                    };
+                            new ServerSentEventsTransport(client),
+                            new LongPollingTransport(client)
+                        };
 
                         var transport = new AutoTransport(client, transports);
 
